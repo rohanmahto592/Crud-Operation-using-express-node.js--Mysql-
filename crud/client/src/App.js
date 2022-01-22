d0c63da6f691cd62b135c4118f9e0e4c2a7bdbec
Rohan Mahto <68339841+rohanmahto592@users.noreply.github.com>
2022-01-22 23:55:00 +0530

    Update App.js

--- crud/client/src/App.js
+++ crud/client/src/App.js
@@ -1,111 +1,174 @@
 
-import './App.css';
-import axios from 'axios'
-import { useState } from 'react';
-import Validation from './Validation';
-function App() {
-//   const [name,setname]= useState(null);
-//   const [age,setage]=useState(0);
-//   const[position,setposition]=useState(null)
-//   const[country,setcountry]=useState(null);
-//   const[wage,setwage]=useState(0);
-//   const[employee,setemployee]=useState([])
-//   const  adduser=((data)=>{
-//   console.log(name,age,position);
-//     axios.post('http://localhost:5000/create',{
-//       name:name,
-//       age:age,
-//       position:position,
-//       country:country,
-//       wage:wage
-//     }).then(()=>{
-//       setemployee([
-//         ...employee,
-//         {
-//           name:name,
-//           age:age,
-//           position:position,
-//           country:country,
-//           wage:wage
-//         }
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
+import axios from "axios";
 
-        
-//       ])
-      
-      
-//     }).catch((err)=>{
-//       console.log(err);
-//     })
-//   })
-// const empdetails=()=>{
-//   axios.get("http://localhost:5000/details").then((response)=>{
-//     console.log(response);
-//     setemployee(response.data);
-    
-//   })
-// }
-// const deletedetails=(id)=>{
-//   axios.delete(`http://localhost:5000/delete/${id}`.then((response)=>{
-//     setemployee(
-//       employee.filter((val)=>{
-//         return val.id!==id
-//       })
-//     );
-//   }))
-// }
+function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  const [newWage, setNewWage] = useState(0);
+
+  const [employeeList, setEmployeeList] = useState([]);
+
+  const addEmployee = () => {
+    Axios.post("http://localhost:3001/create", {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
+      setEmployeeList([
+        ...employeeList,
+        {
+          name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,
+        },
+      ]);
+    });
+  };
+
+  const getEmployees = () => {
+    Axios.get("http://localhost:3001/employees").then((response) => {
+      setEmployeeList(response.data);
+    });
+  };
+
+  const updateEmployeeWage = (id) => {
+    Axios.put("http://localhost:3001/update", { wage: newWage, id: id }).then(
+      (response) => {
+        setEmployeeList(
+          employeeList.map((val) => {
+            return val.id === id
+              ? {
+                  id: val.id,
+                  name: val.name,
+                  country: val.country,
+                  age: val.age,
+                  position: val.position,
+                  wage: newWage,
+                }
+              : val;
+          })
+        );
+      }
+    );
+  };
+  const addddemployess=()=>{
+    Axios.post('http://localhost:30001/postdata',{
+      name:name,
+      age:age,
+      country:country
+    }).then((response)=>{
+      setEmployeeList([{
+        name,age,country
+      }
+      ])
+    })
+
+  }
+
+  const deleteEmployee = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+      setEmployeeList(
+        employeeList.filter((val) => {
+          return val.id !== id;
+        })
+      );
+    });
+  };
+
   return (
     <div className="App">
-      {/* <form method="POST" action="/create"> */}
-      {/* <label>Name:</label><br/>
-      <input type="text" onChange={(event)=>{setname(event.target.value)}}></input><br/>
-      <label>Age:</label><br/>
-      <input type="number" onChange={(event)=>setage(event.target.value)}></input><br/>
-      <label>Position:</label><br/>
-      <input type="text" onChange={(event)=>setposition(event.target.value)}></input><br/>
-      <label>Country:</label><br/>
-      <input type="text" onChange={(event)=>setcountry(event.target.value)}></input><br/>
-      <label>Wage(year):</label><br/>
-      <input type="number" onChange={(event)=>setwage(event.target.value)}></input><br/>
+      <div className="information">
+        <label>Name:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label>Age:</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <label>Country:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        />
+        <label>Position:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        />
+        <label>Wage (year):</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        />
+        <button onClick={addEmployee}>Add Employee</button>
+      </div>
+      <div className="employees">
+        <button onClick={getEmployees}>Show Employees</button>
 
-      <button type="submit" onClick={adduser}> submit</button>
-      
-      <button onClick={empdetails}>showdetails</button>
-      
-      {
-        employee.map((val,id)=>{
-          return(
-            
-            <div>
-              {
-                val.name
-              },
-              {
-                val.age
-              },
-              {
-                val.position
-              },
-              {
-                val.country
-              }
-              {
-                val.wage
-              }
+        {employeeList.map((val, key) => {
+          return (
+            <div className="employee">
+              <div>
+                <h3>Name: {val.name}</h3>
+                <h3>Age: {val.age}</h3>
+                <h3>Country: {val.country}</h3>
+                <h3>Position: {val.position}</h3>
+                <h3>Wage: {val.wage}</h3>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="2000..."
+                  onChange={(event) => {
+                    setNewWage(event.target.value);
+                  }}
+                />
+                <button
+                  onClick={() => {
+                    updateEmployeeWage(val.id);
+                  }}
+                >
+                  {" "}
+                  Update
+                </button>
+
+                <button
+                  onClick={() => {
+                    deleteEmployee(val.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
-            
-
-            
-          )
-          
-            
-           
-        })
-      }
-      */}
-      <Validation/>
-      
-      
-    </div> 
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
